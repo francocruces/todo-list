c78bb3f41f57b7e4ddd74904b8970ed9d4aa2032
Franco Cruces <francocrucesayala@gmail.com>
2018-03-28 16:11:10 +0000

    Implemented add task (closes #1)

--- todoList/todo_list/models.py
+++ todoList/todo_list/models.py
@@ -3,3 +3,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class TodoList(models.Model):
+    
+    def __init__(self):
+        self.tasks = []
+    
+    def is_empty(self):
+        return len(self.tasks) == 0
+
+    def get_tasks(self):
+        return self.tasks
+    
+    def add_task(self, a_task):
+        self.tasks.append(a_task)
--- todoList/todo_list/tests.py
+++ todoList/todo_list/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from todo_list.models import TodoList
 
 # Create your tests here.
+class TodoTests(TestCase):
+    
+    def test_empty_list(self):
+        # given
+        
+        # when
+        todo = TodoList()
+        
+        # then
+        self.assertTrue(todo.is_empty())
+        
+    def test_add_task(self):
+        # given
+        todo = TodoList()
+        
+        # when
+        todo.add_task('hacer un to-do list')
+        
+        # then
+        self.assertFalse(todo.is_empty())
+        self.assertTrue('hacer un to-do list' in todo.get_tasks())
+        
+    def test_add_many_tasks(self):
+        # given
+        todo = TodoList()
+        todo.add_task('hacer un to-do list')
+        
+        # when
+        todo.add_task('hacer otra to-do list')
+        todo.add_task('hacer una to-do list mas')
+        
+        # then
+        self.assertFalse(todo.is_empty())
+        self.assertTrue('hacer otra to-do list' in todo.get_tasks())
+        self.assertTrue('hacer una to-do list mas' in todo.get_tasks())
